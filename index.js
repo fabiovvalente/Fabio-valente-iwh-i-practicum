1360cb75fda2901413a55cecdd6dd1bbe0150613
fabiovvalente <fabiovvalente@gmail.com>
2025-04-09 21:49:03 -0300

    Exercício prático concluído com criação e edição de objetos customizados

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -7,65 +8,91 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// ROTA 1 – Página inicial com listagem dos objetos customizados
+app.get('/', async (req, res) => {
+    const objectTypeId = '2-43140771';
+    const url = `https://api.hubapi.com/crm/v3/objects/${objectTypeId}?properties=name,marca,modelo`;
 
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+    try {
+        const response = await axios.get(url, { headers });
+        const data = response.data.results || [];
+
+        res.render('homepage', {
+            title: 'Lista de Objetos Customizados',
+            data
+        });
+    } catch (error) {
+        console.error('Erro ao buscar objetos:', error.response?.data || error.message);
+        res.status(500).send('Erro ao buscar dados.');
+    }
+});
+
+// ROTA 2 – Página com formulário para criar ou atualizar objeto
+app.get('/update-cobj', async (req, res) => {
+    const objectTypeId = '2-43140771';
+    const id = req.query.id;
+
+    if (!id) {
+        return res.render('updates', { item: null });
     }
 
+    const url = `https://api.hubapi.com/crm/v3/objects/${objectTypeId}/${id}?properties=name,marca,modelo`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const response = await axios.get(url, { headers });
+        res.render('updates', { item: response.data });
+    } catch (error) {
+        console.error('Erro ao buscar objeto para edição:', error.response?.data || error.message);
+        res.status(500).send('Erro ao buscar objeto para edição.');
+    }
 });
-*/
 
+// ROTA 3 – Submissão para criar ou atualizar objeto
+app.post('/update-cobj', async (req, res) => {
+    const objectTypeId = '2-43140771';
+    const { id, name, marca, modelo } = req.body;
 
-// * Localhost
+    if (!name || !marca || !modelo) {
+        return res.status(400).send('Todos os campos são obrigatórios.');
+    }
+
+    const data = {
+        properties: { name, marca, modelo }
+    };
+
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        if (id) {
+            // Atualiza objeto existente
+            const url = `https://api.hubapi.com/crm/v3/objects/${objectTypeId}/${id}`;
+            await axios.patch(url, data, { headers });
+        } else {
+            // Cria novo
+            const url = `https://api.hubapi.com/crm/v3/objects/${objectTypeId}`;
+            await axios.post(url, data, { headers });
+        }
+
+        res.redirect('/');
+    } catch (error) {
+        console.error('Erro ao criar/atualizar objeto:', error.response?.data || error.message);
+        res.status(500).send('Erro ao criar/atualizar objeto.');
+    }
+});
+
+// Inicializa o servidor
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
